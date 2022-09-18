180976cce0a26f5e3b62e56fcbe0bc38806f565b
AntonioVilarim <AntonioVilarim@users.noreply.github.com>
2022-09-18 11:40:49 -0300

    Conclui primeiro exercicio

---
+++ EXERCICIO-1.js
@@ -0,0 +1,11 @@
+nomeCompleto = "Antonio Borges Vilarim Junior"
+apelido = "Tony Vilarim"
+idade = 43
+dataNascimento = "02/08/1979"
+localNascimento = "Paulista"
+altura = 1.80
+trabalhando = true
+
+apresentacao = "Meu nome é " + nomeCompleto + " (sou conhecido[a] como " + apelido + ") e tenho " + idade + " anos. Nasci no dia " + dataNascimento + ", na cidade de " + localNascimento + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + "."
+
+console.log(apresentacao)
